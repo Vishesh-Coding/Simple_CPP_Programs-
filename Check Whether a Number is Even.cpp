e9a2289efa12089379d0816997d717218fad3a45
vishesh <125154385+Vishesh-Coding@users.noreply.github.com>
2023-03-04 23:52:57 +0530

    Create Check Whether a Number is Even.cpp

---
+++ Check Whether a Number is Even.cpp
@@ -0,0 +1,13 @@
+    #include<iostream>
+    using namespace std;
+    int main ()
+    {
+        int num;
+        cout<< "Enter the number  : ";
+        cin>> num;
+        if (num % 2 == 0)
+            cout<< num<< " is Even.";
+        else
+            cout<< num<< " is Not Even ";
+        return 0;
+    }
